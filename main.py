87f5e7423c591c2836eb6b134745b81cb94b0300
Julio Herrera <42546972+jurhs2000@users.noreply.github.com>
2022-02-20 22:00:19 -0600

    multiplicacion con suma 0, pruebas de suma y multiplicacion

--- main.py
+++ main.py
@@ -17,17 +17,23 @@ h = lambda x: lambda y: x**2+y**2
 
 #Sin lambdas de parametros
 cero = lambda f: lambda x: x
-uno = lambda f: lambda x: (f(x))
-dos = lambda f: lambda x: (f(f(x)))
+uno = lambda f: lambda x: f(x)
+dos = lambda f: lambda x: f(f(x))
 tres = lambda f: lambda x: f(f(f(x)))
-
-
-multiplicacion = lambda a: lambda b: lambda f: lambda x: a(b(f))(x)
-print(multiplicacion(uno)(dos)(f)(2))
-
 
 sucesor = lambda n: lambda f: lambda x: f(n(f)(x))
 
 suma = lambda a: lambda b: lambda f: lambda x: a(f)(b(f)(x))
+print('cero + uno + 1 = ', suma(cero)(uno)(f)(1))
+print('cero + dos + 1 = ', suma(cero)(dos)(f)(1))
+print('uno + cero + 5 = ', suma(uno)(cero)(f)(5))
+print('dos + dos + 0 = ', suma(dos)(dos)(f)(0))
+print('dos + tres + 1 = ', suma(dos)(tres)(f)(1))
+print('tres + tres + 1 = ', suma(tres)(tres)(f)(1))
 
-print(suma(uno)(dos)(g)(2))
+multiplicacion = lambda a: lambda b: lambda f: a(b(f))(0) # 0 -> no suma nada a la multiplicación entre los dos numeros
+print('cero * dos = ', multiplicacion(cero)(dos)(f))
+print('uno * dos = ', multiplicacion(uno)(dos)(f))
+print('uno * dos = ', multiplicacion(uno)(dos)(f))
+print('dos * tres = ', multiplicacion(dos)(tres)(f))
+print('tres * tres = ', multiplicacion(tres)(tres)(f))
